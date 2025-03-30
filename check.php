041bd4a3f9d6e5a5dfbc779f462e9f94c7a01fb3
halleisthebest2525 <160375412+halleisthebest2525@users.noreply.github.com>
2025-03-30 16:09:34 +0900

    Update check.php

--- check.php
+++ check.php
@@ -1,1 +1,13 @@
+<?php
+$correct_password = "mypassword"; // Hardcoded password
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $entered_password = $_POST["password"];
+
+    if ($entered_password === $correct_password) {
+        echo "Access Granted!";
+    } else {
+        echo "Access Denied!";
+    }
+}
+?>
